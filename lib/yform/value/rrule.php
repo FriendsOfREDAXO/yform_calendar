b5e09a385c4cb862e2ce460104690b84112225c8
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-01 19:55:25 +0200

    Create rrule.php

---
+++ lib/yform/value/rrule.php
@@ -0,0 +1,41 @@
+<?php
+class rex_yform_value_rrule extends rex_yform_value_abstract
+{
+    function enterObject()
+    {
+        $this->setValue($this->getValue());
+
+        if ($this->getValue() == '' && !$this->params['send']) {
+            $this->setValue($this->getElement('default'));
+        }
+
+        if ($this->needsOutput()) {
+            $this->params['form_output'][$this->getId()] = $this->parse('value.rrule.tpl.php');
+        }
+
+        $this->params['value_pool']['email'][$this->getName()] = $this->getValue();
+        if ($this->saveInDb()) {
+            $this->params['value_pool']['sql'][$this->getName()] = $this->getValue();
+        }
+    }
+
+    function getDescription()
+    {
+        return 'rrule|name|label|';
+    }
+
+    function getDefinitions()
+    {
+        return [
+            'type' => 'value',
+            'name' => 'rrule',
+            'values' => [
+                'name'    => ['type' => 'name',   'label' => rex_i18n::msg('yform_values_defaults_name')],
+                'label'   => ['type' => 'text',   'label' => rex_i18n::msg('yform_values_defaults_label')],
+                'default' => ['type' => 'text',   'label' => rex_i18n::msg('yform_values_rrule_default')],
+            ],
+            'description' => rex_i18n::msg('yform_values_rrule_description'),
+            'dbtype' => 'text'
+        ];
+    }
+}
