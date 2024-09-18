0cf68587813ef73a0cf8e10f1a4ff09f5b7c881d
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 16:34:46 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -13,10 +13,6 @@ if (rex_plugin::get('yform', 'manager')->isAvailable()) {
     rex_yform::addTemplatePath($package->getPath('ytemplates'));
 }
 
-rex_yform_manager_dataset::setModelClass(
-            'rex_yformcalendar',CalRender::class
-);
-
 if (rex::isBackend()) {
     rex_view::addCssFile($this->getAssetsUrl('rrule.css'));
 }
