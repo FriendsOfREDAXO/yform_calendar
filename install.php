55107f7a981320c91951123ca4f76307ae637176
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 16:22:54 +0200

    Delete install.php

--- install.php
+++
@@ -1,4 +0,0 @@
-<?php
-if (rex_addon::get('yform') && rex_addon::get('yform')->isAvailable()) {
-    rex_yform_manager_table_api::importTablesets(rex_file::get(__DIR__ . '/tableset/tableset.json'));
-}
