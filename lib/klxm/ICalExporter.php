1cd204d13a04eb056a7cab5a3dd1a58557595d84
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 23:02:18 +0200

    Update ICalExporter.php

--- lib/klxm/ICalExporter.php
+++ lib/klxm/ICalExporter.php
@@ -6,13 +6,6 @@ use DateTime;
 
 class ICalExporter
 {
-    /**
-     * Generate an iCal file from calendar events.
-     *
-     * @param string $filename The name of the file to generate
-     * @param array $events An array of rex_yform_manager_dataset objects representing the events
-     * @return void
-     */
     public static function generateICalFile(string $filename, array $events): void
     {
         $icalData = self::generateICal($events);
@@ -23,12 +16,6 @@ class ICalExporter
         echo $icalData;
     }
 
-    /**
-     * Generate iCal data from an array of events.
-     *
-     * @param array $events An array of rex_yform_manager_dataset objects
-     * @return string The generated iCal data
-     */
     public static function generateICal(array $events): string
     {
         $ical = "BEGIN:VCALENDAR\r\n";
@@ -44,13 +31,7 @@ class ICalExporter
         return $ical;
     }
 
-    /**
-     * Generate an iCal event string from a single event.
-     *
-     * @param rex_yform_manager_dataset $event The event object
-     * @return string The generated iCal event
-     */
-    private static function generateEvent(rex_yform_manager_dataset $event): string
+    private static function generateEvent($event): string
     {
         $dtStart = self::formatICalDateTime($event->getValue('dtstart'), $event->getValue('all_day'));
         $dtEnd = self::formatICalDateTime($event->getValue('dtend'), $event->getValue('all_day'));
@@ -72,58 +53,17 @@ class ICalExporter
             $icalEvent .= "LOCATION:$location\r\n";
         }
 
-        // Optional: Add RRULE if event is recurring
-        if ($event->getValue('rrule')) {
-            $icalEvent .= "RRULE:" . $event->getValue('rrule') . "\r\n";
-        }
-
-        // Optional: Add EXDATE if event has exceptions
-        if ($event->getValue('exdate')) {
-            $exdates = self::formatICalExDates($event->getValue('exdate'), $event->getValue('all_day'));
-            $icalEvent .= "EXDATE:$exdates\r\n";
-        }
-
         $icalEvent .= "END:VEVENT\r\n";
 
         return $icalEvent;
     }
 
-    /**
-     * Format a date-time string into iCal format.
-     *
-     * @param string $dateTime The date-time string
-     * @param bool $allDay Whether the event is an all-day event
-     * @return string The formatted iCal date-time string
-     */
     private static function formatICalDateTime(string $dateTime, bool $allDay = false): string
     {
         $dt = new DateTime($dateTime);
         return $allDay ? $dt->format('Ymd') : $dt->format('Ymd\THis\Z');
     }
 
-    /**
-     * Format exceptions dates into iCal format.
-     *
-     * @param string $exdateString The exdate string
-     * @param bool $allDay Whether the event is an all-day event
-     * @return string The formatted iCal exdates
-     */
-    private static function formatICalExDates(string $exdateString, bool $allDay = false): string
-    {
-        $exdates = array_map('trim', explode(',', $exdateString));
-        $formattedExDates = array_map(function ($exdate) use ($allDay) {
-            return self::formatICalDateTime($exdate, $allDay);
-        }, $exdates);
-
-        return implode(',', $formattedExDates);
-    }
-
-    /**
-     * Escape special characters in a string for iCal.
-     *
-     * @param string $string The string to escape
-     * @return string The escaped string
-     */
     private static function escapeString(string $string): string
     {
         return preg_replace('/([\,;])/','\\\$1', $string);
