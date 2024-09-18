430227a6ee11d6132d3f8afb88245634c1d9219d
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 17:33:44 +0200

    Update value.rrule.tpl.php

--- ytemplates/bootstrap/value.rrule.tpl.php
+++ ytemplates/bootstrap/value.rrule.tpl.php
@@ -14,22 +14,22 @@ $id = $this->getFieldId();
 
     <div id="<?= $id ?>-widget" class="hidden">
         <div class="form-group">
-            <label for="<?= $id ?>-frequency"><i class="fas fa-clock icon"></i>Häufigkeit:</label>
+            <label for="<?= $id ?>-frequency"><i class="fas fa-clock icon"></i><?= rex_i18n::msg('yform_calendar_frequency') ?>:</label>
             <select id="<?= $id ?>-frequency" class="form-control selectpicker">
-                <option value="DAILY">Täglich</option>
-                <option value="WEEKLY">Wöchentlich</option>
-                <option value="MONTHLY">Monatlich</option>
-                <option value="YEARLY">Jährlich</option>
+                <option value="DAILY"><?= rex_i18n::msg('yform_calendar_daily') ?></option>
+                <option value="WEEKLY"><?= rex_i18n::msg('yform_calendar_weekly') ?></option>
+                <option value="MONTHLY"><?= rex_i18n::msg('yform_calendar_monthly') ?></option>
+                <option value="YEARLY"><?= rex_i18n::msg('yform_calendar_yearly') ?></option>
             </select>
         </div>
         
         <div class="form-group">
-            <label for="<?= $id ?>-interval"><i class="fas fa-step-forward icon"></i>Intervall:</label>
+            <label for="<?= $id ?>-interval"><i class="fas fa-step-forward icon"></i><?= rex_i18n::msg('yform_calendar_interval') ?>:</label>
             <input type="number" id="<?= $id ?>-interval" min="1" value="1" class="form-control">
         </div>
         
         <div id="<?= $id ?>-weekly-group" class="form-group hidden">
-            <label><i class="fas fa-calendar-week icon"></i>Wochentage:</label>
+            <label><i class="fas fa-calendar-week icon"></i><?= rex_i18n::msg('yform_calendar_weekdays') ?>:</label>
             <div class="toggle-group">
                 <?php foreach (['MO', 'TU', 'WE', 'TH', 'FR', 'SA', 'SU'] as $day): ?>
                 <div>
@@ -37,61 +37,61 @@ $id = $this->getFieldId();
                         <input type="checkbox" id="<?= $id ?>-<?= $day ?>">
                         <span class="slider"></span>
                     </label>
-                    <span class="toggle-label"><?= substr($day, 0, 2) ?></span>
+                    <span class="toggle-label"><?= rex_i18n::msg('yform_calendar_' . strtolower($day)) ?></span>
                 </div>
                 <?php endforeach; ?>
             </div>
         </div>
         
         <div id="<?= $id ?>-monthly-group" class="form-group hidden">
-            <label><i class="fas fa-calendar-alt icon"></i>Monatlicher Typ:</label>
+            <label><i class="fas fa-calendar-alt icon"></i><?= rex_i18n::msg('yform_calendar_monthly_type') ?>:</label>
             <div class="radio-group">
-                <label><input type="radio" id="<?= $id ?>-bymonthday" name="<?= $id ?>-monthlyType" value="bymonthday">Tag des Monats</label>
-                <label><input type="radio" id="<?= $id ?>-byday" name="<?= $id ?>-monthlyType" value="byday">Tag der Woche</label>
+                <label><input type="radio" id="<?= $id ?>-bymonthday" name="<?= $id ?>-monthlyType" value="bymonthday"><?= rex_i18n::msg('yform_calendar_day_of_month') ?></label>
+                <label><input type="radio" id="<?= $id ?>-byday" name="<?= $id ?>-monthlyType" value="byday"><?= rex_i18n::msg('yform_calendar_day_of_week') ?></label>
             </div>
         </div>
         
         <div id="<?= $id ?>-bymonthday-group" class="form-group hidden">
-            <label for="<?= $id ?>-monthday"><i class="fas fa-calendar-day icon"></i>Tag des Monats:</label>
+            <label for="<?= $id ?>-monthday"><i class="fas fa-calendar-day icon"></i><?= rex_i18n::msg('yform_calendar_day_of_month') ?>:</label>
             <input type="number" id="<?= $id ?>-monthday" min="1" max="31" value="1" class="form-control">
         </div>
         
         <div id="<?= $id ?>-byday-group" class="form-group hidden">
-            <label><i class="fas fa-calendar-check icon"></i>Wochentag im Monat:</label>
+            <label><i class="fas fa-calendar-check icon"></i><?= rex_i18n::msg('yform_calendar_weekday_of_month') ?>:</label>
             <div class="row">
                 <div class="col-md-6">
                     <select id="<?= $id ?>-weekdayorder" class="form-control selectpicker">
-                        <option value="1">Erster</option>
-                        <option value="2">Zweiter</option>
-                        <option value="3">Dritter</option>
-                        <option value="4">Vierter</option>
-                        <option value="-1">Letzter</option>
+                        <option value="1"><?= rex_i18n::msg('yform_calendar_first') ?></option>
+                        <option value="2"><?= rex_i18n::msg('yform_calendar_second') ?></option>
+                        <option value="3"><?= rex_i18n::msg('yform_calendar_third') ?></option>
+                        <option value="4"><?= rex_i18n::msg('yform_calendar_fourth') ?></option>
+                        <option value="-1"><?= rex_i18n::msg('yform_calendar_last') ?></option>
                     </select>
                 </div>
                 <div class="col-md-6">
                     <select id="<?= $id ?>-weekday" class="form-control selectpicker">
-                        <option value="MO">Montag</option>
-                        <option value="TU">Dienstag</option>
-                        <option value="WE">Mittwoch</option>
-                        <option value="TH">Donnerstag</option>
-                        <option value="FR">Freitag</option>
-                        <option value="SA">Samstag</option>
-                        <option value="SU">Sonntag</option>
+                        <option value="MO"><?= rex_i18n::msg('yform_calendar_monday') ?></option>
+                        <option value="TU"><?= rex_i18n::msg('yform_calendar_tuesday') ?></option>
+                        <option value="WE"><?= rex_i18n::msg('yform_calendar_wednesday') ?></option>
+                        <option value="TH"><?= rex_i18n::msg('yform_calendar_thursday') ?></option>
+                        <option value="FR"><?= rex_i18n::msg('yform_calendar_friday') ?></option>
+                        <option value="SA"><?= rex_i18n::msg('yform_calendar_saturday') ?></option>
+                        <option value="SU"><?= rex_i18n::msg('yform_calendar_sunday') ?></option>
                     </select>
                 </div>
             </div>
         </div>
 
         <div id="<?= $id ?>-end-options" class="form-group">
-            <label><i class="fas fa-calendar-times icon"></i>Ende der Wiederholung:</label>
+            <label><i class="fas fa-calendar-times icon"></i><?= rex_i18n::msg('yform_calendar_end_repeat') ?>:</label>
             <select id="<?= $id ?>-end-type" class="form-control selectpicker">
-                <option value="never">Nie</option>
-                <option value="count">Nach Anzahl der Ereignisse</option>
-                <option value="until">An einem bestimmten Datum</option>
+                <option value="never"><?= rex_i18n::msg('yform_calendar_never') ?></option>
+                <option value="count"><?= rex_i18n::msg('yform_calendar_after_occurrences') ?></option>
+                <option value="until"><?= rex_i18n::msg('yform_calendar_on_date') ?></option>
             </select>
             
             <div id="<?= $id ?>-count-group" class="form-group hidden">
-                <label for="<?= $id ?>-count">Anzahl der Ereignisse:</label>
+                <label for="<?= $id ?>-count"><?= rex_i18n::msg('yform_calendar_number_of_events') ?>:</label>
                 <div class="row">
                     <div class="col-xs-12">
                         <input type="number" id="<?= $id ?>-count" min="1" value="1" class="form-control">
@@ -100,7 +100,7 @@ $id = $this->getFieldId();
             </div>
             
             <div id="<?= $id ?>-until-group" class="form-group hidden">
-                <label for="<?= $id ?>-until">Enddatum:</label>
+                <label for="<?= $id ?>-until"><?= rex_i18n::msg('yform_calendar_end_date') ?>:</label>
                 <div class="row">
                     <div class="col-xs-12">
                         <input type="date" id="<?= $id ?>-until" class="form-control">
