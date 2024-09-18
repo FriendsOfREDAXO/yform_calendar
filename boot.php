d670d2e8f3ba505fc2f3a6ec08e464c903b30aee
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 14:38:29 +0200

    Update boot.php

--- boot.php
+++ boot.php
@@ -1,4 +1,12 @@
 <?php 
+namespace klxm\YFormCalendar;
+
+use rex;
+use rex_addon;
+use rex_plugin;
+use rex_yform;
+use rex_yform_manager_dataset;
+
 $package = rex_addon::get('yform_calendar');
 if (rex_plugin::get('yform', 'manager')->isAvailable()) {
     rex_yform::addTemplatePath($package->getPath('ytemplates'));
