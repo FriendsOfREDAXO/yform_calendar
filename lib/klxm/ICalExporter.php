068f572e8a80c6edb8b9dfe68dc98db82bb783c2
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 23:59:11 +0200

    Update ICalExporter.php

--- lib/klxm/ICalExporter.php
+++ lib/klxm/ICalExporter.php
@@ -21,10 +21,17 @@ class ICalExporter
         $ical = "BEGIN:VCALENDAR\r\n";
         $ical .= "VERSION:2.0\r\n";
         $ical .= "PRODID:-//Your Organization//Your Product//EN\r\n";
-        $ical .= "CALSCALE:GREGORIAN\r\n";
+
+        $processedEvents = [];
 
         foreach ($events as $event) {
-            $ical .= self::generateEvent($event);
+            $eventId = $event->getId();
+            $rrule = $event->getValue('rrule');
+
+            if (!isset($processedEvents[$eventId])) {
+                $ical .= self::generateEvent($event, $rrule);
+                $processedEvents[$eventId] = true;
+            }
         }
 
         $ical .= "END:VCALENDAR\r\n";
@@ -32,81 +39,60 @@ class ICalExporter
         return $ical;
     }
 
-    private static function generateEvent($event): string
+    private static function generateEvent($event, $rrule): string
     {
-        $allDay = $event->getValue('all_day');
-        $dtStart = self::formatICalDateTime($event->getValue('dtstart'), $allDay);
-        $dtEnd = self::formatICalDateTime($event->getValue('dtend'), $allDay);
+        $dtStart = self::formatICalDateTime($event->getValue('dtstart'), $event->getValue('all_day'));
+        $dtEnd = self::formatICalDateTime($event->getValue('dtend'), $event->getValue('all_day'));
         $summary = self::escapeString($event->getValue('summary'));
         $description = self::escapeString($event->getValue('description'));
         $location = self::escapeString($event->getValue('location'));
         $uid = $event->getId();
-        $rrule = $event->getValue('rrule');
 
         $icalEvent = "BEGIN:VEVENT\r\n";
         $icalEvent .= "UID:$uid\r\n";
-        $icalEvent .= "DTSTAMP:" . self::formatICalDateTime(new DateTime(), false) . "\r\n";
-        $icalEvent .= "DTSTART" . ($allDay ? ";VALUE=DATE" : "") . ":$dtStart\r\n";
-        $icalEvent .= "DTEND" . ($allDay ? ";VALUE=DATE" : "") . ":$dtEnd\r\n";
+        $icalEvent .= "DTSTAMP:" . self::formatICalDateTime((new DateTime())->format('Y-m-d H:i:s')) . "\r\n";
+        $icalEvent .= "DTSTART:$dtStart\r\n";
+        $icalEvent .= "DTEND:$dtEnd\r\n";
         $icalEvent .= "SUMMARY:$summary\r\n";
-        $icalEvent .= "DESCRIPTION:$description\r\n";
-        $icalEvent .= "LOCATION:$location\r\n";
-        $icalEvent .= "LAST-MODIFIED:" . self::formatICalDateTime(new DateTime(), false) . "\r\n";
-        $icalEvent .= "SEQUENCE:0\r\n";
-        $icalEvent .= "TRANSP:OPAQUE\r\n";
-
-        if ($rrule) {
-            $icalEvent .= "RRULE:" . self::formatRRule($rrule) . "\r\n";
-            if ($event->getValue('exdate')) {
-                $exdates = self::formatICalExDates($event->getValue('exdate'), $allDay);
-                foreach ($exdates as $exdate) {
-                    $icalEvent .= "EXDATE" . ($allDay ? ";VALUE=DATE" : "") . ":$exdate\r\n";
-                }
-            }
+        if (!empty($description)) {
+            $icalEvent .= "DESCRIPTION:$description\r\n";
+        }
+        if (!empty($location)) {
+            $icalEvent .= "LOCATION:$location\r\n";
         }
 
-        $icalEvent .= self::generateAlarm($allDay);
+        if ($rrule) {
+            $icalEvent .= "RRULE:$rrule\r\n";
+        }
+
+        if ($event->getValue('exdate')) {
+            $exdates = self::formatICalExDates($event->getValue('exdate'), $event->getValue('all_day'));
+            $icalEvent .= "EXDATE:$exdates\r\n";
+        }
 
         $icalEvent .= "END:VEVENT\r\n";
 
         return $icalEvent;
     }
 
-    private static function formatICalDateTime($dateTime, bool $allDay = false): string
+    private static function formatICalDateTime(string $dateTime, bool $allDay = false): string
     {
-        $dt = $dateTime instanceof DateTime ? $dateTime : new DateTime($dateTime);
+        $dt = new DateTime($dateTime);
         return $allDay ? $dt->format('Ymd') : $dt->format('Ymd\THis\Z');
     }
 
-    private static function formatICalExDates(string $exdateString, bool $allDay = false): array
+    private static function formatICalExDates(string $exdateString, bool $allDay = false): string
     {
         $exdates = array_map('trim', explode(',', $exdateString));
-        return array_map(function ($exdate) use ($allDay) {
+        $formattedExDates = array_map(function ($exdate) use ($allDay) {
             return self::formatICalDateTime($exdate, $allDay);
         }, $exdates);
+
+        return implode(',', $formattedExDates);
     }
 
     private static function escapeString(string $string): string
     {
         return preg_replace('/([\,;])/','\\\$1', $string);
-    }
-
-    private static function formatRRule(string $rrule): string
-    {
-        // Vereinfache RRULE für tägliche Wiederholungen
-        if (strpos($rrule, 'FREQ=DAILY;INTERVAL=1') !== false) {
-            return 'FREQ=DAILY';
-        }
-        return $rrule;
-    }
-
-    private static function generateAlarm(bool $allDay): string
-    {
-        $trigger = $allDay ? '-PT15H' : '-PT15M';
-        return "BEGIN:VALARM\r\n" .
-               "ACTION:DISPLAY\r\n" .
-               "DESCRIPTION:Erinnerung\r\n" .
-               "TRIGGER:$trigger\r\n" .
-               "END:VALARM\r\n";
     }
 }
