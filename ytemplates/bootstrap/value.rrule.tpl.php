6add0c98675b02a97cab3295824a36badc5b9ea4
Thomas Skerbis <thomas.skerbis@mac.com>
2024-09-18 17:10:45 +0200

    Update value.rrule.tpl.php

--- ytemplates/bootstrap/value.rrule.tpl.php
+++ ytemplates/bootstrap/value.rrule.tpl.php
@@ -1,114 +1,5 @@
-<?php
-$name = $this->getFieldName();
-$value = $this->getValue();
-$package = rex_addon::get('yform_calendar');
-?>
-<div class="rrule-widget">
-    <label class="toggle-switch">
-        <input type="checkbox" class="rrule-checkbox">
-        <span class="slider"></span>
-    </label>
-
-    <span class="toggle-label"><i class="fas fa-redo icon"></i><?= $this->getLabel() ?></span>
-
-    <div class="rrule-options hidden">
-        <div class="form-group">
-            <label for="rrule-frequency"><i class="fas fa-clock icon"></i><?= $package->i18n('frequency') ?>:</label>
-            <select class="rrule-frequency form-control selectpicker">
-                <option value="DAILY"><?= $package->i18n('daily') ?></option>
-                <option value="WEEKLY"><?= $package->i18n('weekly') ?></option>
-                <option value="MONTHLY"><?= $package->i18n('monthly') ?></option>
-                <option value="YEARLY"><?= $package->i18n('yearly') ?></option>
-            </select>
-        </div>
-        
-        <div class="form-group">
-            <label for="rrule-interval"><i class="fas fa-step-forward icon"></i><?= $package->i18n('interval') ?>:</label>
-            <input type="number" class="rrule-interval form-control" min="1" value="1">
-        </div>
-        
-        <div class="rrule-weekly-group form-group hidden">
-            <label><i class="fas fa-calendar-week icon"></i><?= $package->i18n('weekdays') ?>:</label>
-            <div class="toggle-group">
-                <?php 
-                $days = ['mo', 'tu', 'we', 'th', 'fr', 'sa', 'su'];
-                foreach ($days as $day): 
-                ?>
-                <div>
-                    <label class="toggle-switch">
-                        <input type="checkbox" class="rrule-weekday" data-day="<?= strtoupper($day) ?>">
-                        <span class="slider"></span>
-                    </label>
-                    <span class="toggle-label"><?= $package->i18n($day) ?></span>
-                </div>
-                <?php endforeach; ?>
-            </div>
-        </div>
-        
-        <div class="rrule-monthly-group form-group hidden">
-            <label><i class="fas fa-calendar-alt icon"></i><?= $package->i18n('monthly_type') ?>:</label>
-            <div class="radio-group">
-                <label><input type="radio" name="rrule-monthlyType" value="bymonthday"><?= $package->i18n('day_of_month') ?></label>
-                <label><input type="radio" name="rrule-monthlyType" value="byday"><?= $package->i18n('day_of_week') ?></label>
-            </div>
-        </div>
-        
-        <div class="rrule-bymonthday-group form-group hidden">
-            <label for="rrule-monthday"><i class="fas fa-calendar-day icon"></i><?= $package->i18n('day_of_month') ?>:</label>
-            <input type="number" class="rrule-monthday form-control" min="1" max="31" value="1">
-        </div>
-        
-        <div class="rrule-byday-group form-group hidden">
-            <label><i class="fas fa-calendar-check icon"></i><?= $package->i18n('weekday_of_month') ?>:</label>
-            <div class="row">
-                <div class="col-md-6">
-                    <select class="rrule-weekdayorder form-control selectpicker">
-                        <option value="1"><?= $package->i18n('first') ?></option>
-                        <option value="2"><?= $package->i18n('second') ?></option>
-                        <option value="3"><?= $package->i18n('third') ?></option>
-                        <option value="4"><?= $package->i18n('fourth') ?></option>
-                        <option value="-1"><?= $package->i18n('last') ?></option>
-                    </select>
-                </div>
-                <div class="col-md-6">
-                    <select class="rrule-weekday-select form-control selectpicker">
-                        <option value="MO"><?= $package->i18n('monday') ?></option>
-                        <option value="TU"><?= $package->i18n('tuesday') ?></option>
-                        <option value="WE"><?= $package->i18n('wednesday') ?></option>
-                        <option value="TH"><?= $package->i18n('thursday') ?></option>
-                        <option value="FR"><?= $package->i18n('friday') ?></option>
-                        <option value="SA"><?= $package->i18n('saturday') ?></option>
-                        <option value="SU"><?= $package->i18n('sunday') ?></option>
-                    </select>
-                </div>
-            </div>
-        </div>
-
-        <div class="rrule-end-options form-group">
-            <label><i class="fas fa-calendar-times icon"></i><?= $package->i18n('end_repeat') ?>:</label>
-            <select class="rrule-end-type form-control selectpicker">
-                <option value="never"><?= $package->i18n('never') ?></option>
-                <option value="count"><?= $package->i18n('after_occurrences') ?></option>
-                <option value="until"><?= $package->i18n('on_date') ?></option>
-            </select>
-            
-            <div class="rrule-count-group form-group hidden">
-                <label for="rrule-count"><?= $package->i18n('number_of_events') ?>:</label>
-                <input type="number" class="rrule-count form-control" min="1" value="1" style="width: auto; display: inline-block;">
-            </div>
-            
-            <div class="rrule-until-group form-group hidden">
-                <label for="rrule-until"><?= $package->i18n('end_date') ?>:</label>
-                <input type="date" class="rrule-until form-control" style="width: auto; display: inline-block;">
-            </div>
-        </div>
-    </div>
-    
-    <input type="hidden" class="rrule-value" name="<?= $name ?>" value="<?= htmlspecialchars($value) ?>">
-    <div class="rrule-display hidden"></div>
-</div>
 <script>
-    (function($) {
+(function($) {
     $.fn.rruleWidget = function() {
         return this.each(function() {
             const $widget = $(this);
@@ -141,9 +32,9 @@ $package = rex_addon::get('yform_calendar');
                 toggleVisibility(elements.monthlyGroup, frequency === 'MONTHLY');
                 
                 if (frequency === 'MONTHLY') {
-                    const monthlyType = $widget.find('input[name="rrule-monthlyType"]:checked');
-                    toggleVisibility(elements.bymonthdayGroup, monthlyType.val() === 'bymonthday');
-                    toggleVisibility(elements.bydayGroup, monthlyType.val() === 'byday');
+                    const monthlyType = $widget.find('input[name="rrule-monthlyType"]:checked').val();
+                    toggleVisibility(elements.bymonthdayGroup, monthlyType === 'bymonthday');
+                    toggleVisibility(elements.bydayGroup, monthlyType === 'byday');
                 } else {
                     toggleVisibility(elements.bymonthdayGroup, false);
                     toggleVisibility(elements.bydayGroup, false);
@@ -169,13 +60,11 @@ $package = rex_addon::get('yform_calendar');
                         .join(',');
                     if (weekdays) rrule += `;BYDAY=${weekdays}`;
                 } else if (elements.frequency.val() === 'MONTHLY') {
-                    const monthlyType = $widget.find('input[name="rrule-monthlyType"]:checked');
-                    if (monthlyType.length) {
-                        if (monthlyType.val() === 'bymonthday') {
-                            rrule += `;BYMONTHDAY=${$widget.find('.rrule-monthday').val()}`;
-                        } else if (monthlyType.val() === 'byday') {
-                            rrule += `;BYDAY=${$widget.find('.rrule-weekdayorder').val()}${$widget.find('.rrule-weekday-select').val()}`;
-                        }
+                    const monthlyType = $widget.find('input[name="rrule-monthlyType"]:checked').val();
+                    if (monthlyType === 'bymonthday') {
+                        rrule += `;BYMONTHDAY=${$widget.find('.rrule-monthday').val()}`;
+                    } else if (monthlyType === 'byday') {
+                        rrule += `;BYDAY=${$widget.find('.rrule-weekdayorder').val()}${$widget.find('.rrule-weekday-select').val()}`;
                     }
                 }
                 
@@ -253,11 +142,23 @@ $package = rex_addon::get('yform_calendar');
                 toggleVisibility(elements.rruleWidget, this.checked);
                 toggleVisibility(elements.rruleDisplay, this.checked);
                 updateRRule();
+                updateVisibility();
             });
 
-            elements.frequency.on('change', updateVisibility);
-            $widget.find('input[name="rrule-monthlyType"]').on('change', updateVisibility);
-            elements.endType.on('change', updateVisibility);
+            elements.frequency.on('change', function() {
+                updateVisibility();
+                updateRRule();
+            });
+
+            $widget.find('input[name="rrule-monthlyType"]').on('change', function() {
+                updateVisibility();
+                updateRRule();
+            });
+
+            elements.endType.on('change', function() {
+                updateVisibility();
+                updateRRule();
+            });
 
             $widget.on('change', 'input, select', updateRRule);
 
@@ -275,14 +176,13 @@ $package = rex_addon::get('yform_calendar');
         }
     }
 
-        // Verwendung von jQuery's ready-Funktion für die Initialisierung
-        $(function() {
-            initRRuleWidgets();
-        });
+    // Verwendung von jQuery's ready-Funktion für die Initialisierung
+    $(function() {
+        initRRuleWidgets();
+    });
 
-        // Zusätzlich auf das rex:ready Event hören, falls es in der Umgebung verwendet wird
-        $(document).on('rex:ready', initRRuleWidgets);
+    // Zusätzlich auf das rex:ready Event hören, falls es in der Umgebung verwendet wird
+    $(document).on('rex:ready', initRRuleWidgets);
 
-    })(jQuery);
-
+})(jQuery);
 </script>
